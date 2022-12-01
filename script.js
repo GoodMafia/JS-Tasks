4276f66561dd8dab02b039cb3f602f59d6cdbac3
Daniil <94977641+GoodMafia@users.noreply.github.com>
2022-12-01 19:32:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,178 +1,124 @@
 // Задание 1
-// Перепишите код, заменив цикл for на while, без изменения поведения цикла.
-// for (let i = 0; i < 3; i++) {
-// alert( `number ${i}!` );
-// }
+// Написать функцию, которая принимает два параметра и складывает их.
+// Предусмотреть проверку на тип данных. Если хоть один из параметров не является числом, должно выводиться ообщение об ошибке. Также обраьботайте случай, если не было введено два параметра.
+// Примеры результатов вызова функции:
+// sum(2,4); // 6
+// sum('d',4); // введенные данные не являются числами
+// sum(1, [2]); // введенные данные не являются числами
+// sum(1); // введите два параметра
+// sum(); // введите два параметра
 
-let i = 0;
-while (i < 3){
-    alert(`number ${i}!`);
-    i++
+let sum = function(a, b){
+    if (a / 1 > 0 && b / 1 > 0){
+    return a + b;
+    } else if (typeof a != 'number' || typeof b != 'number'){
+        return 'Введенные данные не являются числами';
+    }
+    else {
+        return 'Введите два параметра';
+    }
 }
-
-
 
 // Задание 2
-// Перепишите код с использованием тернарного оператора
-// let country = 'Sweden';
-// let access;
-// if (country == 'Sweden') {
-// access = true;
-// } else{
-// access = false;
+
+// Измените функцию "square" так, чтобы в случае ее вызова без аргумента генерировалась консольная ошибка (console.error):
+// "Функция "square" не может быть вызвана без аргумента"
+
+// function square(a) {
+//   console.log(a * a)
 // }
 
-let country = 'Sweden';
-let message = country == 'Sweden' ? true : false;
-console.log(message);
+// square(10) // 100
 
+// square()
+// ДО: NaN
+// ПОСЛЕ: Uncaught Error: Функция "square" не может быть вызвана без аргумента
 
+function square(a){
+    if (typeof a != 'number'){
+       console.error(`Uncaught Error: Функция "square" не может быть вызвана без аргумента`);
+    } else {console.log(a * a);}
+}
+
+square(4);
 
 // Задание 3
-// Дан массив логических значений (true/false). Например: const roles = [true, false, false, true, false]
-// Каждое значение обозначает админ (true) или пользователь (false).
-// Из этого массива с помощью цикла сформировать массив объектов - roles, такой же длины, как и заданный массив.  У каждого объекта должно быть одно поле, role, которое равно admin, если значение в заданном массиве - true, и user, если значение в заданном массиве - false.
-// Т.е. из
-// [true, false, false]
-// должно получиться
-// [{ role: 'admin' }, { role: 'user' }, { role: 'user' } ]
-// ПОДСКАЗКИ:
-// 1) создайте пустой массив newRoles.
-// 2) обойдите исходный массив с помощью цикла и, используя тернарный оператор, определите значение роли (admin или user) для каждого элемента массива. Определив это значение (его лучше записывать в переменную), сразу же пушьте в новый массив нужный объект.
-
-const roles = [true, false, false, true, false];
-const newRoles = [];
-for (let i = 0; i < roles.length; i++){
-    let newRole = roles[i] == true ? 'admin' : 'user';
-    newRoles.push({role: newRole});
-}
-console.log(newRoles);
-
-// Задание 4
-// Создайте функцию findPositiveNumbers(). Она принимает массив в качестве параметра и возвращает новый массив, в котором содержатся только положительны элементы.
-// Проверьте работу функции на примере массивов:
-// const nums = [5, 4, 3, -3, -10, -1, 8, -20, 0]
-// const numbers = [443, -39, 0, -105, -21, 38, -20, 3, 90].
-
-const nums = [5, 4, 3, -3, -10, -1, 8, -20, 0];
-function findPositiveNumbers(nums){
-    const positiveNumbers = [];
-    for(let i = 0; i < nums.length; i++){
-        if (nums[i] >= 0){
-            positiveNumbers.push(nums[i]);
-        }
-    }
-        return positiveNumbers;
-}
-console.log(findPositiveNumbers(nums));
-
-
-
-// Задание 5
-
-// Напишите функцию, которая принимает два параметра: значение и количество элементов. Функция создает новый массив и заполняет его этим значением в указанном количестве.
-// Например: из переданных значений ('a', 3) получаем массив ['a', 'a', 'a']
-
-
-function arrayMaster(content, quantity){
-    let newArray = [];
-    for(let i = 0; i < quantity; i++){
-        newArray.push(content);
-    }
-    return newArray;
-}
-console.log(arrayMaster('fas', 2));
-
-
-
-
-// Задание 6
-// Определите массив, например let arr = [5, 4, 3, 8, 0].
-// Создайте функцию filterFor(arr, a). Функция должна вернуть новый массив из элементов arr, но в нем должны содержаться элементы, которые больше или равны (>=) значению переменной a.
-// Например, запуск функции filterFor(arr, 5) дает результат [5,8]
-// а запуск функции filterFor(arr, 10) дает результат []
-// а запуск функции filterFor(arr, 3.11) дает результат [4,5,8]
-
-let arr = [5, 4, 3, 8, 0];
-function filterFor(array, minNumber){
-    let newArr = [];
-    for (let i = 0; i < array.length; i++){
-        if (array[i] > minNumber){
-            newArr.push(array[i]);
-        }
-    }
-    return newArr;
-}
-console.log(filterFor(arr, 3));
-
-
-
-// Задание 7
-// Написать функцию получения случайного цвета. Это будет строка вида "rgb(10,55,250)"
-// Здесь у вас будет две функции: одна ваша - с названием getRandomRGB, а вторая - функция получения случайного числа в диапазоне min - max. Вот эта функция (просто скопируйте, она уже готова):
-// function getRandomInteger(min, max) {
-// return Math.floor(Math.random() * (max - min)) + min;
-// }
-// ПОДСКАЗКИ: 
-// Каждый цвет - это строка, в которой участвуют 3 числа в диапазоне от 0 до 255. 
-// Одна функция может вызывать другую функцию.
+// Создать функцию "угадай число". Она принимает число от 1 до 10 (обязательно проверить, что число не больше 10 и не меньше 0). Генерирует рандомное число от 1 до 10 и сравнивает с заданным числом.
+// Если они совпали, то возвращает “Вы выиграли”, если нет - то “Вы не угадали, ваше число -  ...,  а выпало число ...”
+// Функция создания случайного числа уже была ранее в материалах, в задаче по созданию случайного цвета.
+// Написать функцию в стрелочном синтаксисе.
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function getRandomRGB(){
-    let fistColor = getRandomInteger(0, 255);
-    let secondColor = getRandomInteger(0, 255);
-    let thirdColor = getRandomInteger(0, 255);
-    let result = `rgb(${fistColor}, ${secondColor}, ${thirdColor})`;
-    return result;
-}
 
-console.log(getRandomRGB());
-
-
-
-// Задание 8
-// Написать функцию, которая выводит в консоль квадраты чисел из заданного диапазона (от min до max), где значения min и max передаются в функцию как параметры.
-// Примеры вызова такой функции:
-// getSquares(0, 100) - выведет квадраты чисел от 0 до 100
-// getSquares(56, 87) - выведет квадраты чисел от 56 до 87
-
-function getSquares(min, max){
-    for(let i = min; i <= max; i++){
-        console.log(i ** 2)
+let guessTheNumber = (userNumber) => {
+    let generatedNumber = getRandomInteger(0, 10);
+    if (userNumber < 0 || userNumber > 10){
+        console.log('Нечестная игра!');
+    }
+    else {
+    if (userNumber == generatedNumber){
+        return 'Вы выиграли!';
+    }   else {
+        return `Вы не угадали, ваше число - ${generatedNumber}!`;
+    }
     }
 }
-getSquares(2, 7);
+guessTheNumber(2);
 
-// Задание 9 
-// Написать функцию isInteger(x), которая определяет, является ли число целым, и возвращает true либо false.
-// Для проверки, целое ли число, вам понадобится опеатор % (подумайте, остаток от деления на какое число подойдет для вашей цели)
-
-function isInteger(x){
-    if(x % 1 > 0){
-        return 'false';
-    } else { 
-        return 'true';
+// Задание 4
+// Определить массив, например let arr = [5, 4, 3, 8, 0];
+// Создать функцию filterFor(arr, a). Функция должна вернуть новый массив из элементов arr, но в нем должны содержаться элементы, которые больше или равны (>=) значения переменной a.
+// Например, запуск функции filterFor(arr, 5) дает результат [5,8]
+// запуск функции filterFor(arr, 10) дает результат []
+// запуск функции filterFor(arr, 3.11) дает результат [4,5,8]
+let arr = [5, 4, 3, 8, 0];
+let filterFor = (arr, a) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] > a){
+            newArr.push(arr[i]);
+        }
     }
 }
+filterFor(arr, 5);
 
-console.log(isInteger(15));
-console.log(isInteger(4.5));
+// Задание 5
 
-// Задание 10
-// Написать функцию, которая принимает целое число n.   
-// Внутри функции запустить цикл от 1 до n с шагом 0.5. На каждой итерации цикла выводить в консоль текущее число (i) и информацию о том, целое ли оно.
-// Пример, при запуске функции с параметром 3:
-// "1 integer"
-// "1.5 decimal"
-// "2 integer"
-// "2.5 decimal"
-
-function integerOrDecimal(n){
-    for(let i = 1; i < n; i += 0.5){
-        info = i % 1 == 0 ? 'integer' : 'decimal';
-        console.log(`${i} ${info}`);
-    }
+// Напишите функцию copyArr(arr), которая копирует массив, не изменяя оригинал. Используйте подходящий метод массива.
+let arraytWithNumbers = ['5', 'sdfs', '2', 'fd'];
+let copyArr = (array) => {
+    const newArray = array.map(item => item);
+    return newArray;
 }
-integerOrDecimal(5);
+copyArr(arraytWithNumbers);
+// Задание 6
+
+// Создайте функцию sumObjectValues, которая будет суммировать все значения свойств, которые являются числами. Сумму чисел необходимо вернуть из функции.
+
+
+// Проверить работу функции можно на объекте:
+
+// const objectWithNumbers = {
+//   a: 10,
+//   b: 20,
+//   c: 'string',
+//   d: 12,
+// }
+const objectWithNumbers = {
+  a: 10,
+  b: 20,
+  c: 'string',
+  d: 12,
+}
+let sumObjectValues = (object) => {
+    let sum = 0;
+    for (prop in object){
+        if (typeof object[prop] == 'number'){
+            sum += object[prop] 
+        }
+    }
+    return sum;
+} 
+sumObjectValues(objectWithNumbers);

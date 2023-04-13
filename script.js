228d0c9667d68606ee9171ba391298faa08847a2
Daniil <94977641+GoodMafia@users.noreply.github.com>
2023-04-13 18:43:03 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,210 +1,63 @@
 // Задание 1
-// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
-// Вам понадобятся методы строк.
+// 1)
+// Задать объект с полем name.
+// Задать переменную, которая будет хранить JSON упомянутого объекта.
+// Задать переменную, которая будет хранить результат парсинга предыдущей переменной.
+// 2)
+// Создать переменную user, в которой хранится объект.
+// Записать переменную в localStorage.
+// Создать переменную newUser, в которой будет храниться JS объект из localStorage.
 
-function ucFirst(str) {
-  let newStr = str[0].toUpperCase() + str.slice(1);
-  return newStr;
-}
-
-// Задание 2
-// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
-// Функция должна быть нечувствительна к регистру.
-
-function checkSpam(str) {
-  let newStr = str.toLowerCase();
-  if (str.includes("xxx") || str.includes("badword")) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// 1)
+const obj = { name: "John" };
+const jsonStr = JSON.stringify(obj);
+const parsedObj = JSON.parse(jsonStr);
+// 2)
+const user = { name: "Jane" };
+localStorage.setItem("user", JSON.stringify(user));
+const newUser = JSON.parse(localStorage.getItem("user"));
 
 // Задание 3
-// Написать функцию, которой на вход подается строка, на выход она дает символы наоборот (разворачивает строку). Пример: «привет Женя» -> «янеЖ тевирп»
-// Обратите внимание: метод reverse существует только у массивов.
-
-let string = "Привет Женя";
-let array = string.split("");
-array = array.reverse();
-let newString = array.join("");
-console.log(newString);
-
-// Задание 4
-// Массив содержит строки с информацией о железнодорожных станциях на севере Англии. Строки представляют собой трёхбуквенный код станции, затем некоторые машиночитаемые данные, за которыми следует точка с запятой, а затем название станции, пригодное для чтения человеком.
-// let stations = [
-
-//     'MAN675847583748sjt567654;Manchester Piccadilly',
-    
-//     'GNF576746573fhdg4737dh4;Greenfield',
-    
-//     'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-    
-//     'SYB4f65hf75f736463;Stalybridge',
-    
-//     'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
-    
-//     ];
-    
-    
-//     Необходимо извлечь код станции и имя и поместить их в строку со следующей структурой:
-    
-//     MAN: Manchester Piccadilly
-    
-//     Вывести эти строки в консоль
-    
-//     ПОДСКАЗКА:
-    
-//     1. Извлеките трёхбуквенный код станции и сохраните его в новой переменной.
-//     2. Найдите номер символьного номера точки с запятой.
-//     3. Извлеките название для чтения человеком, используя номер индекса точки с запятой в качестве контрольной точки и сохраните его в новой переменной.
-//     4. Объедините две новые переменные и строк
-
-let stations = [
-'MAN675847583748sjt567654;Manchester Piccadilly',
-'GNF576746573fhdg4737dh4;Greenfield',   
-'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-'SYB4f65hf75f736463;Stalybridge',
-'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
-];
-
-for(let i = 0; i < stations.length; i += 1){
-    let threeLetters = stations[i].slice(0, 3);
-    let index = stations[i].indexOf(';');
-    let forReading = stations[i].slice(index + 1);
-    let wholeString = threeLetters + forReading;
-    console.log(wholeString);
-}
+// Разместить на странице несколько картинок. При клике на любую картинку она плавно становится на бэкграунд всей страницы, а тег картинки получает уникальные css-свойства (чтобы было понятно, что сейчас эта картинка является выбранным элементом). При перезагрузке страницы последнее выбранное состояние должно сохраниться, реализовать через LocalStorage.
+// Пример реализации:
+// https://ucarecdn.com/6e689f37-...
+// ПОДСКАЗКИ
+// - значение атрибута можно узнать с помощью метода element.getAttribute(' '). Но если атрибут стандартный, то можно его забрать с помощью одноименного свойства. Например, адрес картинки = img.src (img - переменная, в которой хранится картика).
+// Порядок работы:
+// 1. Собираем изображения в массив.
+// 2. Вешаем обработчик клика на все окно - window. Так js будет "слушать" клики и записывать в event.target тот элемент, по которому пришелся клик (event нужно обязятельно добавить как параметр в addEventListener).
+// window.addEventListener('click', (event => { }))
+// Перебираем массив картинок, сравниваем их с event.target. Забираем из картинки, на которую пришелся клик, url (значение атрибута src), вешаем на нее класс active, а у остальных картинок убираем. Записываем url активной картинки в память и ставим ее на фон body.
+// При загрузке страницы (в начале кода JS) добавляем проверку, есть ли информация об активной картинке в LocalStorage, если есть - добавляем ей класс active и ставим ее на фон. Если нет - можно присвоить этот класс и поставить на фон первую картинку из массива.
 
 
-// Задание 5
-// Напишите функцию, которая проверяет, можержит ли массив элементы, равные нулю. Если да - возвращает true.
-// Проверить работу функции на примере массивов [12, 4, 50, 1, 0, 18, 40], [56, 24, -55, 2, 9, -345]
 
-function checkArray(array){
-    for(let i = 0; i < array.length; i += 1){
-      if(array[i] == 0){
-        return true;
+
+const body = document.querySelector('body');
+const lastActiveImage = localStorage.getItem('activeImage');
+window.addEventListener('click', (event) => {
+
+    images.forEach((image) => {
+      const img = document.querySelector(`img[src="${image.url}"]`);
+      img.classList.remove('active');
+    });
+
+    event.target.classList.add('active');
+
+    localStorage.setItem('activeImage', url);
+
+        body.style.backgroundImage = url(${url});
+        body.style.backgroundSize = 'cover';
+        body.style.backgroundPosition = 'center';
+    
+        const message = document.createElement('p');
+        message.textContent = alt;
+        body.appendChild(message);
       }
+    });
+    
+
+    if (lastActiveImage) {
+      const img = document.querySelector(img[src="${lastActiveImage}"]);
+      img.click();
     }
-}
-
-
-// Задание 6
-// Напишите функцию unique(arr), которая принимает массив, а возвращает новый массив, содержащий только уникальные элементы arr.
-// Пример:
-// let strings = ["кришна", "кришна", "харе", "харе", "харе", "харе", "кришна", "кришна", ":-O"];
-// console.log(unique(strings) ); должен вывести "кришна, харе, :-O"
-
-let strings = ["кришна", "кришна", "харе", "харе", "харе", "харе", "кришна", "кришна", ":-O"];
-function unique(arr){
-    let newArr = [];
-    for(let i = 0; i < arr.length; i += 1){
-        if (!(newArr.includes(arr[i]))){
-          newArr.push(arr[i]);
-        }
-    }
-}
-
-
-
-// Задание 7
-// Женя и Юля изучают кошек. Каждая из них узнала у 10 владельцев кошек о возрасте их кошки и сохранила данные в массиве (по одному массиву для каждой девочки). На данный момент им просто интересно узнать, является ли кошка взрослой или котёнком.
-// Кошка считается взрослой, если ей не менее 2 лет, и котёнком, если ей менее 2 лет.
-// Создайте функцию verifyCats, которая принимает 2 массива возрастов кошек (catsJane и catsJulia) и выполняет следующие действия:
-// 1. Женя выяснила, что владельцы первой и последней кошки на самом деле имеют собак, а не кошек! Поэтому удалите возраст собак из Жениного массива.
-// 2. Создайте общий массив с данными Жени (исправленными) и Юли.
-// 3. Для каждой кошки этого общего массива выведите в консоль текст:
-// - если кошка взрослая -  Кошка № 1 взрослая, ей 6 лет
-// - если котёнок - Кошка № 2 ещё котёнок
-// 4. Вызовите функцию для двух наборов тестовых данных.
-// Тестовые данные:
-// 1.  Данные Жени [4, 5, 3, 11, 6, 2, 4, 1, 5, 9]
-// Данные Юли [2, 4, 5, 1, 13, 2, 15, 8, 3, 7]
-// 2.  Данные Жени [3, 5, 9, 14, 1, 2, 6, 8, 3, 10]
-// Данные Юли [8, 2, 10, 1, 2, 5, 6, 3, 1, 4]
-
-function verifyCats(catsJane, catsJulia){
-  let newCatsJane = catsJane.slice(1, catsJane.length - 1);
-  let allCats = newCatsJane.concat(catsJulia);
-  for(let i = 0; i < allCats.length; i++){
-    if (allCats[i] >= 2){
-      console.log(`Кошка № ${i + 1} взрослая, ей ${allCats[i]} лет`);
-    } else if (allCats[i] < 2){
-      console.log(`Кошка № ${i + 1} еще котенок`);
-    }
-  }
-}
-verifyCats([4, 5, 3, 11, 6, 2, 4, 1, 5, 9], [2, 4, 5, 1, 13, 2, 15, 8, 3, 7]);
-verifyCats([3, 5, 9, 14, 1, 2, 6, 8, 3, 10], [8, 2, 10, 1, 2, 5, 6, 3, 1, 4]);
-
-
-
-// Задание 8
-// Решить 4 задание из прошлой темы, используя метод filter
-// Определить массив, например let arr = [5, 4, 3, 8, 0];
-// Создать функцию filterFor(arr, a). Функция должна вернуть новый массив из элементов arr, но в нем должны содержаться элементы, которые больше или равны (>=) значения переменной a.
-// Например, запуск функции filterFor(arr, 5) дает результат [5,8]
-// запуск функции filterFor(arr, 10) дает результат []
-// запуск функции filterFor(arr, 3.11) дает результат [4,5,8]
-
-let arr = [5, 4, 3, 8, 0];
-
-function filterFor(arr, a){
-  let newArr = arr.filter(function(item){
-    return item >= a;
-  })
-}
-filterFor(arr, 3);
-
-
-
-// Задание 9
-// Описать функцию, которая принимает массив строк и возвращает массив, содержащий только строки более 3-х символов. Проверить работу функции на примере:  ['yes', 'hello', 'no', 'easycode', 'what'].
-
-
-let strings1 = ['yes', 'hello', 'no', 'easycode', 'what'];
-function stringFilter(arr){
-  let newStrings = strings1.filter(function(item){
-    return item.length > 3;
-  })
-  return newStrings;
-}
-console.log(stringFilter(strings1));
-
-// Задание 10
-// Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [ [14, 45], [1], ['a', 'c', 'd'] ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
-
-let arr1 = [ [14, 45], [1], ['a', 'c', 'd'] ];
-let arr2 = arr1.sort( (a, b) => a.length - b.length);
-console.log(arr2);
-
-// Создайте функцию getAverageHumanAge, которая принимает массив возрастов кошек (catAges) и выполняет следующие действия по порядку:
-// 1. Рассчитывает человеческий возраст по следующей формуле: если возраст кошки <= 2 года, человеческий возраст = возраст кошки * 10. Если кошке больше 2 лет, человеческий возраст = возраст кошки * 7. (сделать через map)
-// 2. Исключает всех кошек младше 18 человеческих лет. (сделать через filter)
-// 3. Возвращает средний человеческий возраст для всех взрослых кошек. (сумму для среднего посчитать через reduce)
-// Вызовите функцию для обоих наборов тестовых данных.
-// Тестовые данные:
-// 1: [7 , 3, 2, 4, 1, 15, 8, 1, 9, 2]
-// 2: [1, 16, 12, 4, 5, 1, 3, 11, 7, 2]
-// В задании необходимо использовать методы массивов: map, filter, reduce
-
-function getAverageHumanAge(catAges){
-  const humanAges = catAges.map(function(item){
-    if(item <= 2){
-      return item * 10;
-    }
-    else if(item > 2){
-      return item * 7;
-    }
-  });
-  const newHumanAges = humanAges.filter(function(item){
-    return item > 10;
-  });
-  const sum = newHumanAges.reduce(function(acc, item, arr){
-    return ( acc + item ) / arr.length;
-  }, 0);
-  console.log(humanAges);  
-  console.log(newHumanAges);
-}
-getAverageHumanAge([7 , 3, 2, 4, 1, 15, 8, 1, 9, 2]);
